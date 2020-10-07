fc1a99ff0c0dbc907bd25e59d40d3dee3ca054f8
qutang <qu.tang@outlook.com>
2020-10-07 12:18:49 -0400

    feat: Save overlayed video, add option to hide or show preview window and progress bar

--- overlay_time_stamp.py
+++ overlay_time_stamp.py
@@ -1,106 +1,134 @@
-from pyffmpeg import FFmpeg
 import sys
 import os
 from os import sep
 import cv2
 import datetime
 from datetime import datetime, timedelta
+import tqdm
 
-# Get the input file folder path
-in_path = sys.argv[1]
 
-# get the file name of the video file
-file_name = sys.argv[2]
+def add_timestamp_overlay(in_path, file_name, out_path=None, preview=True, progress_bar=True):
+    # keep the outpath same as the input path
+    out_path = out_path or in_path
 
-# keep the outpath same as the input path
-out_path = in_path
+    # get the full path of the input file
+    in_file_name = in_path + sep + file_name
 
-# get the full path of the input file
-in_file_name = in_path + sep + file_name
+    # Get the video file into openCV
+    cap = cv2.VideoCapture(in_file_name)
 
-# Get the video file into openCV
-cap = cv2.VideoCapture(in_file_name)
+    # Get the frame rate of the file
+    video_frame_rate = cap.get(cv2.CAP_PROP_FPS)
 
-# Get the frame rate of the file
-video_frame_rate = cap.get(cv2.CAP_PROP_FPS)
+    # Get the number of frames of the file
+    frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
-# File name length
-file_name_len = len(file_name)
+    # Get frame size of the file
+    frame_width, frame_height = int(cap.get(3)), int(cap.get(4))
 
-# remove extension
-video_start_time = file_name[:- 4]
+    # File name length
+    file_name_len = len(file_name)
 
-# Remove the begining QVR_ type string
-video_start_time = video_start_time[4:]
+    # remove extension
+    video_start_time = file_name[:- 4]
 
-# Split the file name by '_' to get date time components
-video_name_string_list = video_start_time.split("_")
+    # Remove the begining QVR_ type string
+    video_start_time = video_start_time[4:]
 
-# Stitch a timestamp
-video_start_time = video_name_string_list[0] + "-" + video_name_string_list[1] + "-" + video_name_string_list[2] + \
-                   " " + \
-                   video_name_string_list[3] + ":" + video_name_string_list[4] + ":" + video_name_string_list[5] + \
-                   ".000"
+    # Split the file name by '_' to get date time components
+    video_name_string_list = video_start_time.split("_")
 
-# Convert to a datetime object
-video_start_time = datetime.strptime(video_start_time, '%Y-%m-%d %H:%M:%S.%f')
+    # Stitch a timestamp
+    video_start_time = video_name_string_list[0] + "-" + video_name_string_list[1] + "-" + video_name_string_list[2] + \
+        " " + \
+        video_name_string_list[3] + ":" + video_name_string_list[4] + ":" + video_name_string_list[5] + \
+        ".000"
 
-print("Start time is: " + str(video_start_time))
+    # Convert to a datetime object
+    video_start_time = datetime.strptime(
+        video_start_time, '%Y-%m-%d %H:%M:%S.%f')
 
-# This line below may not be working. Might be a codec problem
-fourcc = cv2.VideoWriter_fourcc(*'MP4V')
+    print("Start time is: " + str(video_start_time))
 
-# Create an output file name
-output_file = out_path + sep + 'output.mp4'
+    # This line below may not be working. Might be a codec problem
+    fourcc = cv2.VideoWriter_fourcc(*'MP4V')
 
-# Create a video writer buffer
-out = cv2.VideoWriter(output_file, fourcc, 10.0, (640, 480))
+    # Create an output file name
+    output_file = out_path + sep + f'{file_name[:- 4]}_stamped.mp4'
 
-# Initialize the frame count
-frame_count = 0
+    # Create a video writer buffer
+    out = cv2.VideoWriter(output_file, fourcc,
+                          video_frame_rate, (frame_height, frame_width))
 
-print("Video frame rate is: " + str(video_frame_rate))
+    # Initialize the frame count
+    frame_count = 0
 
-while cap.isOpened():
+    print("Video frame rate is: " + str(video_frame_rate))
 
-    # Capture frames in the video
-    ret, frame = cap.read()
+    with tqdm.tqdm(total=frame_count, disable=not progress_bar) as bar:
 
-    # describe the type of font
-    # to be used.
-    font = cv2.FONT_HERSHEY_SIMPLEX
+        while cap.isOpened():
 
-    # Use putText() method for
-    # inserting text on video
+            # Capture frames in the video
+            ret, frame = cap.read()
 
-    # Get number of seconds to ad to the start time fetched from file name
-    secs_add = frame_count / video_frame_rate
-    secs_to_add = timedelta(seconds=secs_add)
-    frame_time = (video_start_time + secs_to_add).strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
-    frame_count = frame_count + 1
-    frame = cv2.putText(frame,
-                        str(frame_time),
-                        (50, 50),
-                        font, 1,
-                        (0, 255, 255),
-                        2,
-                        cv2.LINE_4)
+            # rotate 90 degree counter-clockwise
+            frame = cv2.rotate(frame, cv2.ROTATE_90_COUNTERCLOCKWISE)
 
-    # Display the resulting frame
-    # if end of the frame, exit
-    if not ret:
-        break
+            if ret:
+                bar.update()
+                # describe the type of font
+                # to be used.
+                font = cv2.FONT_HERSHEY_SIMPLEX
 
-    cv2.imshow('video', frame)
-    out.write(frame)
+                # Use putText() method for
+                # inserting text on video
 
-    # creating 'q' as the quit
-    # button for the video
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+                # Get number of seconds to ad to the start time fetched from file name
+                secs_add = frame_count / video_frame_rate
+                secs_to_add = timedelta(seconds=secs_add)
+                frame_time = (video_start_time +
+                              secs_to_add).strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
+                frame_count = frame_count + 1
+                frame = cv2.putText(frame,
+                                    str(frame_time),
+                                    (50, 50),
+                                    font, 1,
+                                    (0, 255, 255),
+                                    2,
+                                    cv2.LINE_4)
+                bar.set_description(frame_time)
+                # Display the resulting frame
+                # if end of the frame, exit
+                if not ret:
+                    break
 
-# release the cap object
-cap.release()
-# close all windows
-cv2.destroyAllWindows()
+                out.write(frame)
 
+                if preview:
+                    cv2.imshow('video', frame)
+
+                # creating 'q' as the quit
+                # button for the video
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+
+            else:
+                break
+
+    # release the cap object
+    cap.release()
+    out.release()
+    # close all windows
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+
+    # Get the input file folder path
+    in_path = sys.argv[1]
+
+    # get the file name of the video file
+    file_name = sys.argv[2]
+
+    add_timestamp_overlay(in_path, file_name, preview=False, progress_bar=True)
