fe4a25fb6c2b0b5b20640c6a01df9b17d6058909
adityaponnada <ponnada.a@husky.neu.edu>
2020-10-07 17:10:00 -0500

    Overlay multiple videos when going through the files

--- overlay_multiple_videos.py
+++ overlay_multiple_videos.py
@@ -1,6 +1,15 @@
 import sys
 import os
 from os import sep
+from overlay_time_stamp import add_timestamp_overlay
 
 in_folder = sys.argv[1]
 
+video_file_list = os.listdir(in_folder)
+
+print('Video files found: ' + str(video_file_list))
+
+for video_file in video_file_list:
+    print('processing: ' + str(video_file))
+    add_timestamp_overlay(in_folder, video_file, None, False, True)
+    # os.system('python overlay_time_stamp.py ' + in_folder + ' ' + str(video_file))
