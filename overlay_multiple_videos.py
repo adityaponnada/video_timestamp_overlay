34e2285bfc1eba87677ffefa27852996e08efcd3
adityaponnada <ponnada.a@husky.neu.edu>
2020-10-07 20:54:33 -0500

    Call video stitching directly from the main timestamp overlay script for multiple files

--- overlay_multiple_videos.py
+++ overlay_multiple_videos.py
@@ -13,3 +13,6 @@ for video_file in video_file_list:
     print('processing: ' + str(video_file))
     add_timestamp_overlay(in_folder, video_file, None, False, True)
     # os.system('python overlay_time_stamp.py ' + in_folder + ' ' + str(video_file))
+
+# Call video stitching
+os.system('python stitch_videos.py ' + str(in_folder))
