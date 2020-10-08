df1fdc000c87de1781b159cad34e10caf846b5cb
qutang <qu.tang@outlook.com>
2020-10-08 12:39:13 -0400

    feat: Use parallel process pool to overlay multiple videos

--- overlay_multiple_videos.py
+++ overlay_multiple_videos.py
@@ -2,17 +2,33 @@ import sys
 import os
 from os import sep
 from overlay_time_stamp import add_timestamp_overlay
+from multiprocessing import Pool
 
-in_folder = sys.argv[1]
+if __name__ == "__main__":
 
-video_file_list = os.listdir(in_folder)
+    in_folder = sys.argv[1]
 
-print('Video files found: ' + str(video_file_list))
+    cores = int(sys.argv[2])
 
-for video_file in video_file_list:
-    print('processing: ' + str(video_file))
-    add_timestamp_overlay(in_folder, video_file, None, False, True)
-    # os.system('python overlay_time_stamp.py ' + in_folder + ' ' + str(video_file))
+    video_file_list = os.listdir(in_folder)
+    video_file_list = list(
+        filter(lambda f: f.endswith('mp4'), video_file_list))
 
-# Call video stitching
-os.system('python stitch_videos.py ' + str(in_folder))
+    print('Video files found: ' + str(video_file_list))
+
+    with Pool(processes=cores) as pp:
+        for video_file in video_file_list:
+            print('processing: ' + str(video_file))
+            stamped_video_file = os.path.join(in_folder, 'stamped', os.path.basename(
+                video_file).replace('.mp4', '_stamped.mp4'))
+            print(stamped_video_file)
+            if os.path.exists(stamped_video_file):
+                print(f'{stamped_video_file} was already stamped, skip it')
+            else:
+                p = pp.apply_async(add_timestamp_overlay, args=(
+                    in_folder, video_file, None, False, True))
+        # os.system('python overlay_time_stamp.py ' + in_folder + ' ' + str(video_file))
+        pp.close()
+        pp.join()
+    # Call video stitching
+    os.system('python stitch_videos.py ' + str(in_folder))
